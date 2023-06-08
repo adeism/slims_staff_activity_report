a99dddf8dbcf64eed70b80ba0b60eede9547e1a7
Ade Ismail Siregar <adeismailbox@gmail.com>
2023-06-08 17:56:09 +0700

    Update staff_act.php

--- staff_act.php
+++ staff_act.php
@@ -1,8 +1,6 @@
 <?php
 /**
- *
  * Copyright (C) 2007,2008  Arie Nugraha (dicarve@yahoo.com)
- *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation; either version 2 of the License, or
@@ -16,12 +14,9 @@
  * You should have received a copy of the GNU General Public License
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
- *
- * 
  */
 
 /* Staff Activity Report Plus Modified  2023 Ade Ismail Siregar | github.com/adeism */
-
 
 // IP based access limitation
 require LIB.'ip_based_access.inc.php';
