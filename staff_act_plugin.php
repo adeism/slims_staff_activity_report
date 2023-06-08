2b0473de8b410e92034b03ecd2d0f228716ca5c2
Ade Ismail Siregar <adeismailbox@gmail.com>
2023-06-08 17:31:02 +0700

    Update staff_act_plugin.php

--- staff_act_plugin.php
+++ staff_act_plugin.php
@@ -1,9 +1,9 @@
 <?php
 /**
- * Plugin Name: Staff Act Report Plus!
+ * Plugin Name: Staff Activity Report Plus
  * Plugin URI: https://github.com/adeism\slims_staff_activity_report
  * Description: when the user updates the biblio, items, and members are entered as counts in the report | ketika user melakukan update biblio, item, dan anggota masuk sebagai hitungan dalam laporan
- * Version: 0.0.1
+ * Version: 0.1
  * Author: Ade Ismail Siregar
  * Author URI: https://github.com/adeism
  */
@@ -12,4 +12,4 @@
 $plugin = \SLiMS\Plugins::getInstance();
 
 // registering our plugin into bibliography module
-$plugin->registerMenu('reporting', __('Staf Report Plus!'), __DIR__ . '/staff_act.php');
+$plugin->registerMenu('reporting', __('Staff Activity Report Plus'), __DIR__ . '/staff_act.php');
